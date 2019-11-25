c078c6ec35422ca7cbc57b515081d167f344aa73
Ilya Katz <ilya@squareup.com>
2019-11-24 20:14:19 -0800

    Create a REIT class

--- chaser.py
+++ chaser.py
@@ -1,10 +1,27 @@
 import robin_stocks as r
 
+class REIT:
+  def __init__(self, symbol, broker):
+    self.symbol=symbol
+    self.bought_price=None
+    self.broker=broker
+
+  def get_details(self):
+    my_stocks = r.build_holdings()
+    for key,value in my_stocks.items():
+      if(key==self.symbol):
+        self.bought_price=value['average_buy_price']
+        print(f"Bought {key} for {self.bought_price}")
+
+
 class Chaser:
   def __init__(self, username, password):
     self.username=username
     self.password=password
     self.login=None
+
+  def reits(self):
+    return ["STWD"]
 
   def _login_required(function):
     def auth(self, *args, **kwargs) :
@@ -16,5 +33,14 @@ class Chaser:
     return auth
 
   @_login_required
+  def broker(self):
+    return r
+
+  @_login_required
   def positions(self):
-    print("#positions")
+    my_stocks = r.build_holdings()
+    for key,value in my_stocks.items():
+      if(key in self.reits()):
+        print(key)
+        reit = REIT(key, self)
+        reit.get_details()
--- runner.py
+++ runner.py
@@ -3,7 +3,5 @@ from chaser import Chaser
 import os
 
 if __name__ == '__main__':
-    print("i'm running")
     chaser = Chaser(os.environ["USER_NAME"], os.environ["PASSWORD"])
-    chaser.positions()
     chaser.positions()
