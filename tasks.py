5172ed19491d38d70029daaec2312c530e35d5a1
Ilya Katz <ilyakatz@gmail.com>
2020-01-06 16:47:36 -0800

    Add new tickers

--- tasks.py
+++ tasks.py
@@ -20,7 +20,7 @@ def dividend_history_worker():
 @celeryd_init.connect
 def dividend_history(conf=None, **kwargs):
   """ https://en.wikipedia.org/wiki/List_of_public_REITs_in_the_United_States """
-  for stock in ["STWD", "AIV", "PLYM", "OUT", "VER", "AGNC", "WPC", "VICI", "MPW", "RWT", "IRM", "LAMR", "AVB", "CPT", "EQR", "ESS", "MAA", "UDR", "COR", "CONE", "DLR", "EQIX", "QTS", "ALX", "CUZ", "EPR"]:
+  for stock in ["STWD", "AIV", "PLYM", "OUT", "VER", "AGNC", "WPC", "VICI", "MPW", "RWT", "IRM", "LAMR", "AVB", "CPT", "EQR", "ESS", "MAA", "UDR", "COR", "CONE", "DLR", "EQIX", "QTS", "ALX", "CUZ", "EPR", "AB", "WMC", "UHT"]:
     print(f"Running worker for {stock}")
     DividendHistory(stock).dump()
   return True
