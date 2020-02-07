357ff0be94bf0f7e1e56ca450c767b9c60e2112d
Ilya Katz <ilyakatz@gmail.com>
2020-02-07 14:10:39 -0800

    Increase yahoo requests batch size

--- dividend_chaser/workers/daily_update_worker.py
+++ dividend_chaser/workers/daily_update_worker.py
@@ -11,7 +11,7 @@ class DailyUpdateWorker:
     time_now = int(datetime.datetime.now().strftime('%s'))
     records = orm.Dividendable.where_raw(f"next_dividend_actual IS false OR next_dividend_date < {time_now}").get()
     stocks = list(map(lambda x: x.symbol, records))
-    chunk_size = 5
+    chunk_size = 10
     for i in range(0, len(stocks), chunk_size):
       chunk = stocks[i:i + chunk_size]
       reload_batch_worker.delay(chunk)
